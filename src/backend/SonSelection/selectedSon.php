74f2c29efe6da313afdda10c5db38140e8f638c1
bull3t <kevin.amaurys@outlook.com>
2024-06-05 23:13:25 -0400

    nada

--- src/backend/SonSelection/selectedSon.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $_SESSION['currentSon'] = $_POST['selectSon'];
-
-    header('Location: ../../../public/views/home.php');
-}
-?>
